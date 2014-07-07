841dbc36d4acd4369f2aa41d99dfb0b0c9ba5c65
prabhasp <prabhas.pokharel@gmail.com>
2014-07-07 16:43:47 -0400

    OpenStreetMap 0.3.1

---
+++ R/OpenStreetMap-package.R
@@ -0,0 +1,22 @@
+# TODO: Add comment
+# 
+# Author: ianfellows
+###############################################################################
+
+
+
+
+#' The United States
+#'
+#' @name states
+#' @docType data
+#' @keywords data
+NULL
+
+#' Places of interest in Los Angeles
+#'
+#' @name LA_places
+#' @docType data
+#' @keywords data
+NULL
+
